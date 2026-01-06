5f26dfd671023e1fc9d8172f8901470660a90848
Muhammad Zeeshan <mzeeshan01017@gmail.com>
2026-01-06 23:36:01 +0500

    mnist classification
    mnist classification

---
+++ Machine_Learning_Projects/Mnist Digit Recognition/app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import numpy as np
+from tensorflow import keras
+from PIL import Image
+
+model = keras.models.load_model("Machine_Learning_Projects/Mnist Digit Recognition/mnist_model.keras")
+
+st.title("MNIST Digit Classifier")
+
+uploaded = st.file_uploader("Upload a digit image (28x28)", type=["png","jpg"])
+
+if uploaded:
+    img = Image.open(uploaded).convert("L").resize((28,28))
+    img_array = np.array(img) / 255.0
+    img_array = img_array.reshape(1,28,28)
+
+    prediction = model.predict(img_array)
+    st.write("Predicted Digit:", np.argmax(prediction))
