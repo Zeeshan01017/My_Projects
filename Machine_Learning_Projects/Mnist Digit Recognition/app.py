0e45a14cf55c80158165c40bbdfb5dc0e4dcd9aa
Muhammad Zeeshan <mzeeshan01017@gmail.com>
2026-02-12 17:44:40 +0500

    house prediction
    house prediction

--- Machine_Learning_Projects/Mnist Digit Recognition/app.py
+++ Machine_Learning_Projects/Mnist Digit Recognition/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 from tensorflow import keras
 from PIL import Image
-
+from PIL import Image, ImageOps
 model = keras.models.load_model("Machine_Learning_Projects/Mnist Digit Recognition/mnist_model.keras")
 
 st.title("MNIST Digit Classifier")
@@ -10,9 +10,13 @@ st.title("MNIST Digit Classifier")
 uploaded = st.file_uploader("Upload a digit image (28x28)", type=["png","jpg"])
 
 if uploaded:
-    img = Image.open(uploaded).convert("L").resize((28,28))
-    img_array = np.array(img) / 255.0
-    img_array = img_array.reshape(1,28,28)
+    img = Image.open(uploaded).convert("L")
+    img = ImageOps.invert(img)          
+    img = img.resize((28, 28))
+
+    img_array = np.array(img)
+    img_array = img_array / 255.0
+    img_array = img_array.reshape(1, 28, 28)
 
     prediction = model.predict(img_array)
     st.write("Predicted Digit:", np.argmax(prediction))
